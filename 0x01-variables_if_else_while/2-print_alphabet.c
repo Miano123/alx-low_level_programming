c05145be6dd981057fa03525bf49b97310ec13c6
Miano123 <rachelmiano91@gmail.com>
2022-04-08 11:18:24 -0700

    My task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - pprints the alphabet in lowercase followed bya new line
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	chr ch;
+
+	for (ch = 'a'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
