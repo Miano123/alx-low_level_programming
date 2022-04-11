96cae26de964253a73ca29f5ea67b6ffb998068d
Miano123 <rachelmiano91@gmail.com>
2022-04-11 10:42:51 -0700

    My task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,17 @@
 
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
 
-	printf("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of an int:%lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int:%lu byet(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int:%lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float:%lu byte(s)\n", (unsigned long)sizeof(f));
+
+	
+	printf("Size of int: %zu bytes\n", sizeof(intType))
+	printf("Size of float: %zu byte\n", sizeof(floatType));
+	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+        print("Size of char: %zu bytes\n", sizeof(charType));
+
 	return (0);
 }
