76e18b0359d9a6fe4dddebb16e4929f748f142ad
Miano123 <rachelmiano91@gmail.com>
2022-04-13 07:57:48 -0700

    My files

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include  "main.h"
+
+/**
+
+   * print_alphabet_x10 - function that prints 10 times the alphabet,
+
+    * in lowercase
+
+     * Return: nothing.
+
+      */
+
+void print_alphabet_x10(void)
+{
+	int 1;
+	int n = 0;
+	while (n < 10)
+	{
+		1 = 'a';
+
+		while (1 <= 'z')
+		{
+			_putchar(1);
+
+			1 += 1;
+		}
+		_putchar(10);
+		n += 1;
+	}
+}
