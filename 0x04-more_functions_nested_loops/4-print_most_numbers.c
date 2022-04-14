75a679578b80acf5dc86bb642ea9a4a802200f21
Miano123 <rachelmiano91@gmail.com>
2022-04-14 16:28:45 -0700

    My task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,15 +5,21 @@
   *
   * Retrun: nothing
   */
+
 void print_most_numbers(void)
 {
-	int n = 0;
+	int i;
+	 
+	i = 0;
 
-	while (n < 10)
+
+	while (i < 10)
 	{
 		if (n  != 2 && n != 4)
-			_putchar(n + '0');
-		n += 1;
+		{
+			_putchar(i + '0');
+		}
+		i++;
 	}
-	_putchar('\n');
+	_putchar ('\n');
 }
