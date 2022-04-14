4eacd23092f4ead00b547a7a2511eaba8e57c63b
Miano123 <rachelmiano91@gmail.com>
2022-04-14 15:44:46 -0700

    My task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -11,10 +11,9 @@ void print_numbers(void)
 	int i;
 
 	i = 0;
-	while (i < 10)
-	{
-		_putchar(i + '0');
-		i++;
-	}
-	_putchar('\n');
+	for (i = o; i < 10; i++)
+
+		_putchar(i = '0');
+
+	_putchar ('\n');
 }
