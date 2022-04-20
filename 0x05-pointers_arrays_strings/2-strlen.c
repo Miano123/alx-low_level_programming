27a680e6e6bea84f184839667c74acb34926e242
Miano123 <rachelmiano91@gmail.com>
2022-04-20 13:40:55 -0700

    My task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *int_strlen - funtcion that returns the length of a string
+  *@s: Char
+  *Return: Always 0
+  */
+
+int_strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
