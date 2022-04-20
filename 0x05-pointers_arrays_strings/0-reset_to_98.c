e6eb442ac828de9f597bf9b103c93ff9d5bd91b0
Miano123 <rachelmiano91@gmail.com>
2022-04-20 12:47:16 -0700

    My task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *reset_to_98 - updates value to it points to 98
+  *@n: A pointer that will be updated
+  *Return: Void to mean correct
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+My readme file for C pointers,arrays and strings
