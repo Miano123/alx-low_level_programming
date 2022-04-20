11442d14e9690b56c1d26c7df813abbcc35b6b7c
Miano123 <rachelmiano91@gmail.com>
2022-04-20 15:29:22 -0700

    My task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *print_rev - prints a string rev
+  *@s: point to be updated
+  *Return:0
+  */
+
+void print_rev(char *s)
+{
+	int a;
+
+	a = 0;
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	for (a = a - a; a >= 0; a--)
+	{
+		putchar(s[a]);
+	}
+	putchar('\n');
+}
