20cae0b06df264346a7bc1ebc66b14c7f83f5b95
Miano123 <rachelmiano91@gmail.com>
2022-04-20 15:47:48 -0700

    My task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include  <stdio.h>
 
 /**
   *print_rev - prints a string rev
@@ -17,7 +18,7 @@ void print_rev(char *s)
 	}
 	for (a = a - a; a >= 0; a--)
 	{
-		_putchar(s[a]);
+		putchar(s[a]);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
