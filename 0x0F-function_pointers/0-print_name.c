92a28c3091c3c6f1e7a7a0e4d2b45fdcf68a4c6e
Miano123 <rachelmiano91@gmail.com>
2022-05-17 04:46:07 -0700

    Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+  *print_name - prints a name
+  *@name: input name
+  *@f: function pointer
+  *Return:0 Always
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
